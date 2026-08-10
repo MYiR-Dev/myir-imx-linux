601221f03acc9609c5521c3164127eecb9bfee0e
wujl <1053924360@qq.com>
2026-08-10 19:10:50 +0800

    FEAT: add NVP6324 Jaguar1 decoder

---
+++ drivers/media/i2c/jaguar1_drv/jaguar1_v4l2.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * jaguar1 driver
+ *
+ */
+#ifndef _JAGUAR1_V4L2_H_
+#define _JAGUAR1_V4L2_H_
+
+int nvp6324_sensor_mod_init(void);
+void jaguar1_reconfig_active_ch(void);
+
+#endif
